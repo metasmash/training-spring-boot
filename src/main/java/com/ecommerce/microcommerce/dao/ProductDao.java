13146928839e8838543bb1dbf4b1774e39e31d16
OUALI YANNICK <yo@mydatamodels.com>
2020-10-18 11:56:07 +0200

    define const with different REQUEST (with cool naming ofc)

--- src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
@@ -11,15 +11,24 @@ import java.util.List;
 @Repository
 public interface ProductDao extends JpaRepository<Product, Integer> {
 
+    // query params
+    public static final String TRI_ORDRE_ALPHABETIQUE = "SELECT id, nom, prix, prixAchat FROM Product p ORDER BY p.nom";
+    public static final String MARGE_PRODUIT = "SELECT id, nom, prix, prixAchat, prix-prixAchat FROM Product";
+    public static final String CHERCHER_PRODUIT_CHER = "SELECT id, nom, prix FROM Product p WHERE p.prix > :prixLimit";
+
+
     Product findById(int id);
 
     List<Product> findByPrixGreaterThan(int prixLimit);
 
     List<Product> findByNomLike(String recherche);
 
-    @Query("SELECT id, nom, prix FROM Product p WHERE p.prix > :prixLimit")
+    @Query(CHERCHER_PRODUIT_CHER)
     List<Product>  chercherUnProduitCher(@Param("prixLimit") int prix);
 
-    @Query("SELECT id, nom, prix, prixAchat, prix-prixAchat FROM Product")
-    List<Product> calculerLaMarge();
+    @Query(MARGE_PRODUIT)
+    List<Product> calculerMargeProduit();
+
+    @Query(TRI_ORDRE_ALPHABETIQUE)
+    List<Product> trierProduitParOrdreAlphabetique();
 }
